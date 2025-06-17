a3df36d8bbce8602d8d5ce4bc02b69959a4bb3b9
Adam <adam@lumesoftware.com>
2025-06-17 14:35:53 +0100

    add updateReceiptMovement and updateHazardousWaste routes to router

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,12 +1,19 @@
 import { health } from '../routes/health.js'
 import { createReceiptMovement } from '../routes/create-receipt-movement.js'
+import { updateReceiptMovement } from '../routes/update-receipt-movement.js'
+import { updateHazardousWaste } from '../routes/update-hazardous-waste.js'
 
 const router = {
   plugin: {
     name: 'router',
     register: async (server, _options) => {
       // Register all routes
-      const routes = [health, createReceiptMovement]
+      const routes = [
+        health,
+        createReceiptMovement,
+        updateReceiptMovement,
+        updateHazardousWaste
+      ]
 
       // Register routes directly
       server.route(routes)
