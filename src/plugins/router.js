5e18a21dc631bafa90bd15138e911d3f405a2bd9
David Shepherd <6839214+shepda@users.noreply.github.com>
2025-10-07 13:02:10 +0100

    DWT-717 Add EWC Codes Reference Data GET Endpoint (#96)
    * DWT-717 Add EWC Codes Reference Data GET Endpoint
    
    * DWT-717 Update the EWC Code integration test assertions
    
    * DWT-717 Remove debug log from EWC codes endpoint

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,13 +1,19 @@
 import { health } from '../routes/health.js'
 import { createReceiptMovement } from '../routes/create-receipt-movement.js'
 import { updateReceiptMovement } from '../routes/update-receipt-movement.js'
+import { getEwcCodes } from '../routes/reference-data/get-ewc-codes.js'
 
 const router = {
   plugin: {
     name: 'router',
     register: async (server, _options) => {
       // Register all routes
-      const routes = [health, createReceiptMovement, updateReceiptMovement]
+      const routes = [
+        health,
+        createReceiptMovement,
+        updateReceiptMovement,
+        getEwcCodes
+      ]
 
       // Register routes directly
       server.route(routes)
