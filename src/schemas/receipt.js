b98f592f5f10411f9e1c24112b8e46d63e648ca7
David Shepherd <david@x-designs.co.uk>
2025-10-15 17:59:57 +0100

    DWT-797 Add path property to the custom reasonForNoConsignmentCode as the custom validation is attached to the parent object

--- src/schemas/receipt.js
+++ src/schemas/receipt.js
@@ -143,11 +143,15 @@ export const receiveMovementRequestSchema = Joi.object({
 
     if (hasHazardous && !value.hazardousWasteConsignmentCode) {
       if (!value.reasonForNoConsignmentCode) {
-        return helpers.error('reasonForNoConsignmentCode.required')
+        return helpers.error('reasonForNoConsignmentCode.required', {
+          path: ['reasonForNoConsignmentCodes']
+        })
       }
 
       if (!NO_CONSIGNMENT_REASONS.includes(value)) {
-        return helpers.error('reasonForNoConsignmentCode.only')
+        return helpers.error('reasonForNoConsignmentCode.only', {
+          path: ['reasonForNoConsignmentCodes']
+        })
       }
     }
 
