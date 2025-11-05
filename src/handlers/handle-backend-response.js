2e1f73cebe2ff75143ab7a95099442b59ca0c495
David Shepherd <david@x-designs.co.uk>
2025-11-05 11:45:40 +0000

    DWT-910 Remove debug logging from handleBackendResponse()

--- src/handlers/handle-backend-response.js
+++ src/handlers/handle-backend-response.js
@@ -1,17 +1,7 @@
 import { HTTP_STATUS } from '../common/constants/http-status-codes.js'
-import { createLogger } from '../common/helpers/logging/logger.js'
 import { isSuccessStatusCode } from '../common/helpers/utils.js'
 
-const logger = createLogger()
-
 export function handleBackendResponse(response, h, responseBodyFn) {
-  logger.debug(`handleBackendResponse response.error ${response.error}`)
-  logger.debug(`handleBackendResponse response.message ${response.message}`)
-  logger.debug(
-    `handleBackendResponse response.statusCode ${response.statusCode}`
-  )
-  logger.debug(`handleBackendResponse response ${JSON.stringify(response)}`)
-
   if (isSuccessStatusCode(response.statusCode)) {
     const successStatusCode =
       response.statusCode === HTTP_STATUS.CREATED
