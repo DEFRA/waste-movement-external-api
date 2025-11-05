ce6e569b030dd9eb9c3a57ff30a9e15cdb696cf8
David Shepherd <6839214+shepda@users.noreply.github.com>
2025-11-05 11:06:22 +0000

    DWT-910 Add debug logging for the Backend service response (#122)

--- src/handlers/handle-backend-response.js
+++ src/handlers/handle-backend-response.js
@@ -1,7 +1,17 @@
 import { HTTP_STATUS } from '../common/constants/http-status-codes.js'
+import { createLogger } from '../common/helpers/logging/logger.js'
 import { isSuccessStatusCode } from '../common/helpers/utils.js'
 
+const logger = createLogger()
+
 export function handleBackendResponse(response, h, responseBodyFn) {
+  logger.debug(`handleBackendResponse response.error ${response.error}`)
+  logger.debug(`handleBackendResponse response.message ${response.message}`)
+  logger.debug(
+    `handleBackendResponse response.statusCode ${response.statusCode}`
+  )
+  logger.debug(`handleBackendResponse response ${JSON.stringify(response)}`)
+
   if (isSuccessStatusCode(response.statusCode)) {
     const successStatusCode =
       response.statusCode === HTTP_STATUS.CREATED
