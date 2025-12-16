a73da7eebd76e29dfa1d22432cf23741d6a8c970
David Shepherd <david@x-designs.co.uk>
2025-12-16 14:33:07 +0000

    DWT-1292 Update Disposal or Recovery Code constant structure so additional fields can be added

--- src/common/constants/treatment-codes.js
+++ src/common/constants/treatment-codes.js
@@ -1,37 +1,94 @@
-// Recovery codes enum
 const RECOVERY_CODES = [
-  'R1', // Use principally as a fuel or other means to generate energy
-  'R2', // Solvent reclamation/regeneration
-  'R3', // Recycling/reclamation of organic substances which are not used as solvents
-  'R4', // Recycling/reclamation of metals and metal compounds
-  'R5', // Recycling/reclamation of other inorganic materials
-  'R6', // Regeneration of acids or bases
-  'R7', // Recovery of components used for pollution abatement
-  'R8', // Recovery of components from catalysts
-  'R9', // Oil re-refining or other reuses of oil
-  'R10', // Land treatment resulting in benefit to agriculture or ecological improvement
-  'R11', // Use of wastes obtained from any of the operations numbered R1 to R10
-  'R12', // Exchange of wastes for submission to any of the operations numbered R1 to R11
-  'R13' // Storage of wastes pending any of the operations numbered R1 to R12
+  {
+    code: 'R1'
+  },
+  {
+    code: 'R2'
+  },
+  {
+    code: 'R3'
+  },
+  {
+    code: 'R4'
+  },
+  {
+    code: 'R5'
+  },
+  {
+    code: 'R6'
+  },
+  {
+    code: 'R7'
+  },
+  {
+    code: 'R8'
+  },
+  {
+    code: 'R9'
+  },
+  {
+    code: 'R10'
+  },
+  {
+    code: 'R11'
+  },
+  {
+    code: 'R12'
+  },
+  {
+    code: 'R13'
+  }
 ]
 
-// Disposal codes enum
 const DISPOSAL_CODES = [
-  'D1', // Deposit into or onto land
-  'D2', // Land treatment
-  'D3', // Deep injection
-  'D4', // Surface impoundment
-  'D5', // Specially engineered landfill
-  'D6', // Release into a water body except seas/oceans
-  'D7', // Release into seas/oceans including sea-bed insertion
-  'D8', // Biological treatment not specified elsewhere
-  'D9', // Physico-chemical treatment not specified elsewhere
-  'D10', // Incineration on land
-  'D11', // Incineration at sea
-  'D12', // Permanent storage
-  'D13', // Blending or mixing prior to submission to any of the operations numbered D1 to D12
-  'D14', // Repackaging prior to submission to any of the operations numbered D1 to D13
-  'D15' // Storage pending any of the operations numbered D1 to D14
+  {
+    code: 'D1'
+  },
+  {
+    code: 'D2'
+  },
+  {
+    code: 'D3'
+  },
+  {
+    code: 'D4'
+  },
+  {
+    code: 'D5'
+  },
+  {
+    code: 'D6'
+  },
+  {
+    code: 'D7'
+  },
+  {
+    code: 'D8'
+  },
+  {
+    code: 'D9'
+  },
+  {
+    code: 'D10'
+  },
+  {
+    code: 'D11'
+  },
+  {
+    code: 'D12'
+  },
+  {
+    code: 'D13'
+  },
+  {
+    code: 'D14'
+  },
+  {
+    code: 'D15'
+  }
 ]
 
-export const DISPOSAL_OR_RECOVERY_CODES = [...RECOVERY_CODES, ...DISPOSAL_CODES]
+export const DISPOSAL_OR_RECOVERY_CODES = [
+  ...RECOVERY_CODES.map(({ code }) => code),
+  ...DISPOSAL_CODES.map(({ code }) => code)
+]
