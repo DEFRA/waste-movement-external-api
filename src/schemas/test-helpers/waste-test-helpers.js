8b913d5a080d30d616d53ea9a969489b53038d48
abey onalaja <abey.onalaja@gmail.com>
2026-06-12 15:04:52 +0100

    Update `waste-movement-utils` to latest version and remove unused `TEST_CONSTANTS` export.

--- src/schemas/test-helpers/waste-test-helpers.js
+++ src/schemas/test-helpers/waste-test-helpers.js
@@ -1,7 +1,5 @@
 import { createTestPayloadWith } from 'waste-movement-utils'
 import { createMovementRequest } from '../../test/utils/createMovementRequest.js'
 
-export { TEST_CONSTANTS } from 'waste-movement-utils'
-
 export const createTestPayload = (overrides) =>
   createTestPayloadWith(createMovementRequest, overrides)
