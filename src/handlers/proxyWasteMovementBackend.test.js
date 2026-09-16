785fe6629c3cfb6055e52bb5af9dcffefc81ad70
ee-pbradley <paul.bradley@equalexperts.com>
2026-09-16 14:39:17 +0100

    proxy - pass allowed headers

--- src/handlers/proxyWasteMovementBackend.test.js
+++ src/handlers/proxyWasteMovementBackend.test.js
@@ -32,7 +32,8 @@ describe('proxyWasteMovementBackend', () => {
   const h = {
     response: jest.fn().mockReturnThis(),
     code: jest.fn().mockReturnThis(),
-    message: jest.fn().mockReturnThis()
+    message: jest.fn().mockReturnThis(),
+    header: jest.fn().mockReturnThis()
   }
 
   it('should successfully call the waste movement backend with a valid payload', async () => {
@@ -49,6 +50,36 @@ describe('proxyWasteMovementBackend', () => {
     expect(httpClients.wasteMovement.post).toHaveBeenCalledWith(path, payload)
     expect(h.response).toHaveBeenCalledWith(backendResponse.payload)
     expect(h.code).toHaveBeenCalledWith(backendResponse.statusCode)
+  })
+
+  it('should successfully manage headers to proxy', async () => {
+    const backendResponse = {
+      statusCode: HTTP_STATUS.CREATED,
+      payload: { movementId: 'movementId' },
+      statusMessage: 'Successful',
+      headers: {
+        'Content-Type': 'application/problem+json',
+        'x-request-id': '12345678',
+        'back-end-header': 'xxx'
+      }
+    }
+
+    httpClients.wasteMovement.post.mockResolvedValue(backendResponse)
+
+    await proxyWasteMovementBackend(goodRequest, h)
+
+    expect(httpClients.wasteMovement.post).toHaveBeenCalledWith(path, payload)
+    expect(h.response).toHaveBeenCalledWith(backendResponse.payload)
+    expect(h.code).toHaveBeenCalledWith(backendResponse.statusCode)
+    expect(h.header).toHaveBeenCalledWith(
+      'Content-Type',
+      'application/problem+json'
+    )
+    expect(h.header).toHaveBeenCalledWith('x-request-id', '12345678')
+    expect(h.header).not.toHaveBeenCalledWith(
+      'back-end-header',
+      expect.anything()
+    )
   })
 
   it('should return 500 when waste collection creation fails', async () => {
