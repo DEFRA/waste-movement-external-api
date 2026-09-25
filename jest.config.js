2da1ca5fc36db3f9ab076ee1b3923a8b5ac71d04
abey onalaja <abey.onalaja@gmail.com>
2026-09-25 16:58:25 +0100

    Upgrade `@defra/waste-movement-utils` to v1.13.0 and add support for ESM-only `@joi/date` in Jest config

--- jest.config.js
+++ jest.config.js
@@ -20,14 +20,15 @@ export default {
   ],
   coverageDirectory: '<rootDir>/coverage',
   transform: {
-    '^.+\\.js$': 'babel-jest'
+    '^.+\\.(js|mjs)$': 'babel-jest'
   },
   transformIgnorePatterns: [
     `node_modules/(?!${[
       '@defra/hapi-tracing', // Supports ESM only
       'node-fetch', // Supports ESM only
       'uuid',
-      '@defra/waste-movement-utils'
+      '@defra/waste-movement-utils',
+      '@joi/date' // Supports ESM only
     ].join('|')}/)`
   ]
 }
